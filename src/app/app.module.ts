81914e693f9e6b41ba2a60cf5c4845fc2b7fe76d
amirgolp <44730575+amirgolp@users.noreply.github.com>
2022-12-24 19:36:04 +0100

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ProductsComponent } from "./products.component";
+import { ProductComponent } from './components/product/product.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductsComponent
+    ProductsComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule
